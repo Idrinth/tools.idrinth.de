fc04cc3a8844948110ab1a7b3d5913b3211d9a47
Björn Büttner <Idrinth@users.noreply.github.com>
2023-02-20 16:40:55 +0100

    Update config.php

--- config.php
+++ config.php
@@ -4,3 +4,6 @@ $GLOBALS['db'] = new mysqli('localhost','tools','','tools');
 $GLOBALS['db']->set_charset('utf8');
 
 $GLOBALS['hostname'] = 'tools.idrinth.de';
+$GLOBALS['mailaccount'] = '';
+$GLOBALS['mailpassword'] = '';
+$GLOBALS['mailserver'] = '';
